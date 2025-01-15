e1235cc2a7e784a52c0c12fb7a9722891dc072fd
kimlore <loresariego@gmail.com>
2025-01-15 13:35:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementByld("myButton").addEventListener("click", function() {alert("Button clicked!");
+});
+document.getElementByld("myButton").addEventListener("click",function(){
+    window.location.hresf="file:///C:/Users/MONMON/newpage.html";});
